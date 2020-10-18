61ea4eb26f805aeb99156f724073e323e05192d4
Carlos Sesma <usarral@github.com>
2020-10-18 21:58:35 +0200

    BEAUTIFY CODE

--- JS/pedidoajax.js
+++ JS/pedidoajax.js
@@ -1,13 +1,13 @@
-  window.addEventListener("DOMContentLoaded", function() {
+  window.addEventListener("DOMContentLoaded", function () {
 
     // get the form elements defined in your form HTML above
-    
+
     var form = document.getElementById("my-form");
     var button = document.getElementById("my-form-button");
     var status = document.getElementById("my-form-status");
 
     // Success and Error functions for after the form is submitted
-    
+
     function success() {
       form.reset();
       button.style = "display: none ";
@@ -20,20 +20,20 @@
 
     // handle the form submission event
 
-    form.addEventListener("submit", function(ev) {
+    form.addEventListener("submit", function (ev) {
       ev.preventDefault();
       var data = new FormData(form);
       ajax(form.method, form.action, data, success, error);
     });
   });
-  
+
   // helper function for sending an AJAX request
 
   function ajax(method, url, data, success, error) {
     var xhr = new XMLHttpRequest();
     xhr.open(method, url);
     xhr.setRequestHeader("Accept", "application/json");
-    xhr.onreadystatechange = function() {
+    xhr.onreadystatechange = function () {
       if (xhr.readyState !== XMLHttpRequest.DONE) return;
       if (xhr.status === 200) {
         success(xhr.response, xhr.responseType);
--- JS/pedidos.js
+++ JS/pedidos.js
@@ -2,75 +2,81 @@ var bolsas = 0;
 var pulseras = 0;
 var camisetas = 0;
 var loteria = 0;
-var resumen = ""    ;
+var resumen = "";
 
 
 
-document.getElementById('bolsas').innerHTML= bolsas;
-document.getElementById('pulseras').innerHTML= pulseras;
-document.getElementById('camisetas').innerHTML= camisetas;
-document.getElementById('loteria').innerHTML= loteria;
+document.getElementById('bolsas').innerHTML = bolsas;
+document.getElementById('pulseras').innerHTML = pulseras;
+document.getElementById('camisetas').innerHTML = camisetas;
+document.getElementById('loteria').innerHTML = loteria;
 
 
 
 
 /* ==================FUNCIONES==============================*/
 
-function anadir_bolsas(){
-    bolsas=bolsas+1;
-    document.getElementById('bolsas').innerHTML= bolsas;
-    
-    
+function anadir_bolsas() {
+    bolsas = bolsas + 1;
+    document.getElementById('bolsas').innerHTML = bolsas;
+
+
 };
-function restar_bolsas(){
-    if (bolsas>0) {
-        bolsas=bolsas-1;
-        var resumen= [bolsas, pulseras, camisetas, loteria];
+
+function restar_bolsas() {
+    if (bolsas > 0) {
+        bolsas = bolsas - 1;
+        var resumen = [bolsas, pulseras, camisetas, loteria];
 
     }
-    document.getElementById('bolsas').innerHTML= bolsas;
-    
+    document.getElementById('bolsas').innerHTML = bolsas;
+
 };
-function anadir_pulseras(){
-    pulseras=pulseras+1;
-    document.getElementById('pulseras').innerHTML= pulseras;
-    
+
+function anadir_pulseras() {
+    pulseras = pulseras + 1;
+    document.getElementById('pulseras').innerHTML = pulseras;
+
 };
-function restar_pulseras(){
-    if (pulseras>0) {
-        pulseras=pulseras-1;
+
+function restar_pulseras() {
+    if (pulseras > 0) {
+        pulseras = pulseras - 1;
     }
-    
-    
-    document.getElementById('pulseras').innerHTML= pulseras;
-    
+
+
+    document.getElementById('pulseras').innerHTML = pulseras;
+
 };
-function anadir_camisetas(){
-    camisetas=camisetas+1;
-    document.getElementById('camisetas').innerHTML= camisetas;
-    
+
+function anadir_camisetas() {
+    camisetas = camisetas + 1;
+    document.getElementById('camisetas').innerHTML = camisetas;
+
 };
-function restar_camisetas(){
-    if (camisetas>0) {
-        camisetas=camisetas-1
+
+function restar_camisetas() {
+    if (camisetas > 0) {
+        camisetas = camisetas - 1
     }
-    document.getElementById('camisetas').innerHTML= camisetas;
-    
+    document.getElementById('camisetas').innerHTML = camisetas;
+
 };
-function anadir_loteria(){
-    loteria=loteria+1;
-    document.getElementById('loteria').innerHTML= loteria;
-    
+
+function anadir_loteria() {
+    loteria = loteria + 1;
+    document.getElementById('loteria').innerHTML = loteria;
+
 };
-function restar_loteria(){
-    if (loteria>0){
-        loteria=loteria-1;
+
+function restar_loteria() {
+    if (loteria > 0) {
+        loteria = loteria - 1;
     }
-    document.getElementById('loteria').innerHTML= loteria;
-    
+    document.getElementById('loteria').innerHTML = loteria;
+
 };
 
 
 
 /*=================================================================*/
-
